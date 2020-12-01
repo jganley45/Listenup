b2221d9b0a4bf534cab7ff768765a92bafb92da8
Joe Ganley <jganley45@gmail.com>
2020-12-01 15:32:00 -0500

    Initial Commit

---
+++ ListenUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListenUp
+//
+//  Created by Joe Ganley on 12/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListenUp/ListenUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListenUpApp.swift
+//  ListenUp
+//
+//  Created by Joe Ganley on 12/1/20.
+//
+
+import SwiftUI
+
+@main
+struct ListenUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
